362049d849deb0c4f755fcbd107f9a936abc2707
Матиев Магомед Мусаевич <mmmatiev@edu.hse.ru>
2024-11-04 17:16:49 +0000

    Matiev m

--- preprocess_data/preprocess.py
+++ preprocess_data/preprocess.py
@@ -1,14 +1,59 @@
-def normalize_density(data):
+import numpy as np
+import cv2
+import os
+
+
+def compute_dataset_mean_std(dataset_dir):
     """
-    Нормализует плотность данных.
+    Вычисляет среднее и стандартное отклонение для всего датасета изображений в формате .jpg.
+
+    Args:
+        dataset_dir (str): Путь к директории с изображениями.
+
+    Returns:
+        tuple: Среднее и стандартное отклонение по всему датасету.
+    """
+    sum_pixels = 0
+    sum_squares = 0
+    total_pixels = 0
+
+    for filename in os.listdir(dataset_dir):
+        file_path = os.path.join(dataset_dir, filename)
+
+        if filename.endswith('.jpg'):
+            image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+            if image is None:
+                continue
+
+            image = image.astype(np.float32)
+
+            sum_pixels += image.sum()
+            sum_squares += np.square(image).sum()
+
+            total_pixels += image.size
+
+    mean = sum_pixels / total_pixels
+
+    std = np.sqrt((sum_squares / total_pixels) - mean ** 2)
+
+    return mean, std
+
+
+def normalize_density(data, mean, std):
+    """
+    Нормализует плотность данных с использованием Z-нормализации на основе
+    глобального среднего и стандартного отклонения всего датасета.
 
     Args:
         data (numpy.ndarray): Входные данные, представляющие собой массив плотности.
+        mean (float): Среднее значение для всего датасета.
+        std (float): Стандартное отклонение для всего датасета.
 
     Returns:
         numpy.ndarray: Нормализованные данные.
     """
 
+    normalized_data = (data - mean) / std
     return normalized_data
 
 
@@ -27,7 +72,7 @@ def filter_noise(data, sigma=1):
     return filtered_data
 
 
-def preprocess(data, sigma=1):
+def preprocess(data, mean, std, sigma=1):
     """
     Выполняет предварительную обработку данных, включая нормализацию и фильтрацию шумов.
 
@@ -38,11 +83,10 @@ def preprocess(data, sigma=1):
     Returns:
         numpy.ndarray: Данные после обработки.
     """
-    normalized = normalize_density(data)
+    normalized = normalize_density(data, mean, std)
     filtered = filter_noise(normalized, sigma)
     return filtered
 
 
 if __name__ == "__main__":
-
     pass
