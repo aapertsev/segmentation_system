56e6d452b4f81884ea2db0a4675dc938c7ad3aa7
Перцев Александр Анатольевич <aapertsev@edu.hse.ru>
2024-11-04 17:33:06 +0000

    Исправлены ошибки аргументов

--- preprocess_data/dataset.py
+++ preprocess_data/dataset.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
-from .preprocess import preprocess  # импорт функции предобработки
+from .preprocess import preprocess, compute_dataset_mean_std  # импорт функции предобработки
 
 class CTScanDataset(Dataset):
     def __init__(self, images_dir, masks_dir, transform=None, sigma=1):
@@ -12,6 +12,7 @@ class CTScanDataset(Dataset):
         self.image_filenames = os.listdir(images_dir)
         self.transform = transform
         self.sigma = sigma
+        self.mean, self.std = compute_dataset_mean_std(self.images_dir)
 
     def __len__(self):
         return len(self.image_filenames)
@@ -21,7 +22,7 @@ class CTScanDataset(Dataset):
         image_path = os.path.join(self.images_dir, image_name)
         image = Image.open(image_path).convert('L')
         image = np.array(image)
-        processed_image = preprocess(image, sigma=self.sigma)
+        processed_image = preprocess(image, self.mean, self.std, sigma=self.sigma)
 
         mask = np.zeros(processed_image.shape, dtype=np.uint8)
         for label, suffix in zip([1, 2, 3], ['-AO', '-COR', '-PA']):
