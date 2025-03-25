db90266982260c21f683295181b552987d3bc27f
Pertsev Aleksandr <aapertsev@edu.hse.ru>
2025-03-26 02:09:44 +0300

    +установка стримлит

--- install_files/install_linux.sh
+++ install_files/install_linux.sh
@@ -14,4 +14,5 @@ export nnUNet_raw="${PROJECT_ROOT}/nnUNet_raw"
 export nnUNet_preprocessed="${PROJECT_ROOT}/nnUNet_preprocessed"
 export nnUNet_results="${PROJECT_ROOT}/nnUNet_results"
 cd ..
+pip install streamlit
 echo "Done!"
