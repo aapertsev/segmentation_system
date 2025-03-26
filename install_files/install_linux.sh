b8a184ffa90d4f48b29ccad1ee6c2866c8cfd573
Pertsev Aleksandr <aapertsev@edu.hse.ru>
2025-03-26 12:09:51 +0300

    обновление установочного скрипта

--- install_files/install_linux.sh
+++ install_files/install_linux.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-PROJECT_ROOT="$(cd "$(dirname "$0/..")" && pwd)"
+
+# Определяем директорию, в которой лежит скрипт
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+# Теперь поднимаемся на уровень выше
+PROJECT_ROOT="$(cd "$SCRIPT_DIR"/.. && pwd)"
 echo "Project root = $PROJECT_ROOT"
 echo "Installing Python requirements..."
 pip install -r "${PROJECT_ROOT}/requirements.txt"
 echo "Installing nnUNet in editable mode..."
+cd "${PROJECT_ROOT}"
 git clone https://github.com/aapertsev/nnUNet.git
 cd "${PROJECT_ROOT}/nnUNet"
 pip install -e .
@@ -13,6 +18,6 @@ echo "Exporting nnU-Net environment variables..."
 export nnUNet_raw="${PROJECT_ROOT}/nnUNet_raw"
 export nnUNet_preprocessed="${PROJECT_ROOT}/nnUNet_preprocessed"
 export nnUNet_results="${PROJECT_ROOT}/nnUNet_results"
-cd ..
+cd "${PROJECT_ROOT}"
 pip install streamlit
 echo "Done!"
